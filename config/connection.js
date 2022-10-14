02efcfdf7d29e98095a2345daf0a634f87803795
Devajith <devajithmr@gmail.com>
2022-10-14 18:44:27 +0530

    design change mongoConnection

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const state={
 
 module.exports.connects=function(callback)
 {
-    const url="mongodb://localhost:27017"
+    const url="mongodb+srv://devajith:MJC8tjq167Pktqwk@cluster0.jzonz9t.mongodb.net/?retryWrites=true&w=majority"
     const dbName="shoppingCart"
 
      mongoClient.connect(url,(err,data)=>
